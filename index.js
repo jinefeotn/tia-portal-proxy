e05dd96ff7462480854d8544a405d13d601a3a61
jinefeotn <nebizenginsony@gmail.com>
2025-04-13 03:41:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Statik dosyaları sunmak için 'public' klasörü ayarlıyoruz
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Ana sayfa
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.send('Merhaba, bu jinefeotn tarafından güncellenen bir projedir!');
 });
 
 app.listen(port, () => {
-  console.log(`Sunucu http://localhost:${port} adresinde çalışıyor`);
+  console.log(`Sunucu ${port} portunda çalışıyor.`);
 });
