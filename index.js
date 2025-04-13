a6e452818fee0c289061492457fd1fc7036b5b17
jinefeotn <nebizenginsony@gmail.com>
2025-04-13 04:00:31 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
+
+app.use('/tiaportal', createProxyMiddleware({
+    target: 'http://localhost:5112', // Yerel sunucu adresi
+    changeOrigin: true,
+    pathRewrite: {
+        '^/tiaportal': '/en-US/tiaportal/home' // Yerel yol
+    },
+    onProxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('api', 'V20'); // ?api=V20 parametresini ekle
+    }
+}));
 
 app.use(express.static(path.join(__dirname)));
-
 app.get('/newsitemap.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'newsitemap.html'));
 });
